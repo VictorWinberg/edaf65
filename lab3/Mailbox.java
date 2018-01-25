ceede1ea1f4fc49d58ed5a29290ebe388ce15e10
VictorWinberg <dat14vwi@student.lu.se>
2018-01-25 20:17:21 +0100

    [lab3] add mailbox sync threads

---
+++ lab3/Mailbox.java
@@ -0,0 +1,40 @@
+package lab3;
+
+import lab3.WriteThread;
+import lab3.ReadThread;
+
+public class Mailbox {
+  
+  private String value;
+  
+  public Mailbox() {
+    value = "Initialized";
+  }
+  
+  public synchronized void set(String value) throws InterruptedException {
+    while(this.value != null) {
+      wait();
+    }
+    this.value = value;
+  }
+  
+  public synchronized String get() throws InterruptedException {
+    while(value == null) {
+      wait();
+    }
+    String temp = value;
+    value = null;
+    notifyAll();
+    return temp;
+  }
+  
+  public static void main(String[] args) {
+    Mailbox mailbox = new Mailbox();
+    for (int i = 0; i < 10; i++) {
+      WriteThread thread = new WriteThread("Thread " + (i + 1), mailbox);
+      thread.start();
+    }
+    ReadThread read = new ReadThread(mailbox);
+    read.start();
+  }
+}
---
+++ lab3/ReadThread.java
@@ -0,0 +1,25 @@
+package lab3;
+
+import lab3.Mailbox;
+
+public class ReadThread extends Thread {
+
+  private Mailbox mailbox;
+
+  public ReadThread(Mailbox mailbox) {
+    this.mailbox = mailbox;
+  }
+  
+  @Override
+  public void run() {
+    while(true) {
+      try {
+        sleep((long) Math.random());
+        String value = mailbox.get();
+        System.out.println(value);
+      } catch (InterruptedException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+  }
+}
