b777128540c9394264734ae615282135e045e6ea
VictorWinberg <dat14vwi@student.lu.se>
2018-01-25 19:04:53 +0100

    [lab3] add Threads

---
+++ lab3/Threads.java
@@ -0,0 +1,29 @@
+package lab3;
+
+public class Threads extends Thread {
+
+  private String name;
+
+  public Threads(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      try {
+        sleep((long) Math.random());
+      } catch (InterruptedException e) {
+        System.out.println(e.getMessage());
+      }
+      System.out.println(name);
+    }
+  }
+  
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      Threads thread = new Threads("Thread " + (i + 1));
+      thread.start();
+    }
+  }
+}
